efe56a3c3871c713940b907747b4c3372008f63b
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-04-12 11:03:37 -0300

    feat:adicionando parte dos livros

--- components/container.tsx
+++ components/container.tsx
@@ -5,7 +5,10 @@ interface ContainerProps extends ComponentProps<"section"> {}
 export function Container({ children, className }: ContainerProps) {
   return (
     <section
-      className={twMerge("w-full max-w-[1440px]   sm:px-8  m-auto ", className)}
+      className={twMerge(
+        "w-full max-w-[1440px]  px-2 sm:px-8  m-auto ",
+        className
+      )}
     >
       {children}
     </section>
